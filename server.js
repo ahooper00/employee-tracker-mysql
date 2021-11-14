82340663f15b36e94511f5fa89bb391fe267839f
Amelia Hooper <ajhooper97@gmail.com>
2021-11-15 10:33:13 +1100

    create sql tables

--- server.js
+++ server.js
@@ -1,1 +1,26 @@
 const inquirer = require('inquirer');
+
+const firstQuestionPrompt = () => {
+    return inquirer.prompt([
+        {
+            type: 'list',
+            name: 'options',
+            message: 'What would you like to do?',
+            choices: ['View all departments', 'View all roles', 'View all employees', 'Add a department', 'Add a role', 'Add an employee', 'Update an employee role']
+        }
+    ]).then
+}
+
+// const firstQuestionPrompt = () => {
+//     return inquirer.prompt([
+//         {
+//             type: 'list',
+//             name: 'members',
+//             message: `Do you want to add a team member?`,
+//             choices: ['Yes', 'No'],
+//         }
+//     ]).then(answers => {
+//         const addTeamMember = answers.members;
+//         return addTeamMember === 'Yes'
+//     })
+// }

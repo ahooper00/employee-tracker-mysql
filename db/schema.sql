ff367aae9374fd0e062607f90de1f02645b169bf
Amelia Hooper <ajhooper97@gmail.com>
2021-11-15 13:06:59 +1100

    connecting to database

--- db/schema.sql
+++ db/schema.sql
@@ -1,5 +1,5 @@
-DROP DATABASE IS EXISTS company_db;
-CREATE DATABASE IF EXISTS company_db;
+DROP DATABASE IF EXISTS company_db;
+CREATE DATABASE company_db;
 
 USE company_db;
 
@@ -10,9 +10,12 @@ CREATE TABLE department (
 
 CREATE TABLE employee_role (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    department_id INT NOT NULL
+    department_id INT,
     title VARCHAR(30) NOT NULL,
-    salary DECIMAL(10,2), 
+    salary DECIMAL(10,2),
+    FOREIGN KEY (department_id)
+    REFERENCES department(id)
+    ON DELETE SET NULL
 );
 
 CREATE TABLE employee (
@@ -20,5 +23,7 @@ CREATE TABLE employee (
     role_id INT NOT NULL,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
-    manager_id INT
+    manager_id INT,
+    FOREIGN KEY (role_id)
+    REFERENCES employee_role(id)
 );
--- server.js
+++ server.js
@@ -1,4 +1,33 @@
 const inquirer = require('inquirer');
+const express = require('express');
+const mysql = require('mysql2');
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+// Express middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+// Connect to database
+const db = mysql.createConnection(
+    {
+      host: 'localhost',
+      // MySQL username,
+      user: 'root',
+      // TODO: Add MySQL password here
+      password: 'password',
+      database: 'company_db'
+    },
+    console.log(`Connected to the movies_db database.`)
+  );
+
+
+
+
+
+
+
 
 const firstQuestionPrompt = () => {
     return inquirer.prompt([

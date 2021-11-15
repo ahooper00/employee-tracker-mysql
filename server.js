1e9eacd07e9d69ff516c6261529dfd541240e62e
Amelia Hooper <ajhooper97@gmail.com>
2021-11-15 21:39:08 +1100

    updateEmployee function created

--- server.js
+++ server.js
@@ -193,8 +193,8 @@ const getDepartments = () => new Promise((resolve, reject) => {
 const addEmployee = async () => {
     try {
         console.log('ADD NEW EMPLOYEE');
-        const employees = await getEmployees();
-        const employeeChoices = employees.map(empl => empl.title)
+        const role = await getRoles();
+        const roleChoices = role.map(empl => empl.title)
 
         let inputEmployee = await inquirer.prompt([
             {
@@ -211,10 +211,10 @@ const addEmployee = async () => {
                 type: 'list',
                 name: 'role',
                 message: 'What is the role of the new employee?',
-                choices: employeeChoices,
+                choices: roleChoices,
             }
         ]);
-        let employeeRole = employees.find(empl => empl.title === inputEmployee.role).id;
+        let employeeRole = role.find(empl => empl.title === inputEmployee.role).id;
         await connection.query('INSERT INTO employee SET ?', {
             role_id: employeeRole,
             first_name: inputEmployee.firstName,
@@ -227,7 +227,7 @@ const addEmployee = async () => {
     }
 }
 
-const getEmployees = () => new Promise((resolve, reject) => {
+const getRoles = () => new Promise((resolve, reject) => {
     let query = 'SELECT * FROM employee_role';
     connection.query(query, function (err, res) {
         if (err) reject(err);
@@ -237,3 +237,50 @@ const getEmployees = () => new Promise((resolve, reject) => {
         resolve(employeeList);
     });
 });
+
+const updateEmployeeRole = async () => {
+    try {
+        console.log('UPDATE EXISTING EMPLOYEE ROLE');
+        const employees = await getEmployees();
+        const roles = await getRoles();
+        const employeeChoices = employees.map(empl => `${empl.first_name} ${empl.last_name}`);
+        const roleChoices = roles.map(role => role.title);
+
+        let updateEmployee = await inquirer.prompt([
+            {
+                type: 'list',
+                name: 'name',
+                message: 'Which employee would you like to update?',
+                choices: employeeChoices,
+            },
+            {
+                type: 'list',
+                name: 'newRole',
+                message: 'What would you like to update the role to?',
+                choices: roleChoices,
+            }
+        ]);
+        let employee = employees.find(empl => `${empl.first_name} ${empl.last_name}` === updateEmployee.name);
+        let employeeRoleId = roles.find(role => role.title === updateEmployee.newRole).id;
+
+        await connection.query(`UPDATE employee SET role_id = ? WHERE id = ?`, [
+            employeeRoleId,
+            employee.id
+        ]);
+        console.log(`Role update!`)
+        beginApplication();
+    } catch (err) {
+        console.log(err)
+    }
+}
+
+const getEmployees = () => new Promise((resolve, reject) => {
+    let query = 'SELECT * FROM employee';
+    connection.query(query, function (err, res) {
+        if (err) reject(err);
+        let employeeList = [];
+        res.forEach(employee => employeeList.push(employee));
+        console.log(employeeList);
+        resolve(employeeList);
+    });
+})
